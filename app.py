31a31236e4ea989d5ed8d16a9b1324f3b60bfa8c
Mathias Moen <mathiasbbmoen@gmail.com>
2026-03-03 14:27:20 +0100

    Got flask connected

--- app.py
+++ app.py
@@ -7,5 +7,13 @@ def index():
     
     return render_template("index.html")
 
+@app.route("/submit", methods=["POST"])
+def test():
+
+    username = request.form.get("username")
+    password = request.form.get("password")
+    print(username, password)
+    return f"Welcome, {username}!"
+
 if __name__ == "__main__":
     app.run(debug=True)

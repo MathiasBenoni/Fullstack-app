44c2e84fd139199259aba5a696654ac8f8b03a30
Mathias Moen <mathiasbbmoen@gmail.com>
2026-03-10 10:28:26 +0100

    A little visual queue once you have logged in

--- app.py
+++ app.py
@@ -44,5 +44,12 @@ def signed_up():
 
     return render_template("index.html")
 
+@app.route("/logout")
+def logout():
+    session.clear()
+    return redirect(url_for("index"))
+
+
+
 if __name__ == "__main__":
     app.run(debug=True)

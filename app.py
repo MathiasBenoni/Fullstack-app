f46d2e712f04289302b65a8267e254ff5c19b34f
Mathias Moen <mathiasbbmoen@gmail.com>
2026-03-10 12:17:38 +0100

    Klar for eksamen

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 from flask import *
 from database_python import *
 from password_hashing import prep_database
+from google.oauth2 import id_token
+from google.auth.transport import requests as grequests
+
 
 app = Flask(__name__)
 app.secret_key = "NOPE"
@@ -9,6 +12,22 @@ app.secret_key = "NOPE"
 def index():
     
     return render_template("index.html")
+
+@app.route("/google_login", methods=["POST"])
+def google_login():
+    token = request.json.get("token")
+    try:
+        idinfo = id_token.verify_oauth2_token(
+            token,
+            grequests.Request(),
+            "303838859659-1aa84a95l3jibim48c7c918q5aq573cj.apps.googleusercontent.com"
+        )
+        session["username"] = idinfo["email"]
+        session["privileges"] = "NONE"
+        return jsonify({"success": True})
+    except Exception as e:
+        return jsonify({"success": False, "error": str(e)})
+
 
 @app.route("/signup")
 def signup():
